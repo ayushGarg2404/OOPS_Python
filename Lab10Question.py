769650ce483b5b4b70f74bcd3fde56df34e1c8d2
Ayush Garg <ayushgargofficial24@gmail.com>
2025-11-05 11:09:14 +0530

    Add file writing for calculated percentage

--- Lab10Question.py
+++ Lab10Question.py
@@ -2,6 +2,8 @@ try:
     marks = int(input("Marks: "))
     total_marks = int(input("Totat marks: "))
     percentage = marks/total_marks*100
+    with open('ForStore.txt','w') as f:
+        f.write(str(percentage))
 except ValueError:
     print("Enter a valid number")
 except ZeroDivisionError:
